496c56a7dcb9b520e13081aff19a761dc8fbad1d
AnilGupta001 <ga6119480@gmail.com>
2024-03-05 20:26:43 +0530

    day

---
+++ Basic/src/com/grocerindia/ProjectTesting.java
@@ -0,0 +1,16 @@
+package com.grocerindia;
+
+public class ProjectTesting {
+public static void main(String args[]) {
+	System.out.print("Welcom to NewProgram");
+}
+}
+//
+/*
+ * 
+ * 
+ * 
+ * 
+ * 
+ * */
+ */
